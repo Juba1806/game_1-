469c01f2d73586b08b4d7d1751c318133674f250
Aissaoui Mohamed <109305362+Juba1806@users.noreply.github.com>
2022-11-13 12:39:26 +0000

    Update ship.py

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
     def __init__(self, my_game1):
         """Intialize the sip and set its starting positiong."""
         self.screen = my_game1.screen
+        self.simo = my_game1.simo
         self.screen_rect = my_game1.screen.get_rect()
 
         # Load the ship image and get its rect
@@ -19,11 +20,25 @@ class Ship:
         # Start each new shit at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
+
+        # Moving the flag 
+        self.moving_right = False
+        self.moving_left = False
+
+
+    def update(self):
+        """UPdate the ship's position based on the flag movement """
+        if self.moving_right and self.rect.right < self.screen_rect.right + 80 :
+            self.x += self.simo.ship_speed
+        if self.moving_left and self.rect.left > -80:
+            self.x -= self.simo.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x
+
 
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
-~                                                                                            
-~                                                                                            
-~                                                                                            
-~                                                          
